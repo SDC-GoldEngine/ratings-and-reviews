fec4e8906bde0610aae18ecd7a7704c88d3fe07e
Jessica Zhou <jessicazhou541@gmail.com>
2022-06-15 14:10:31 -0700

    added loader io path

--- db/index.js
+++ db/index.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 const pool = new Pool ({
-  user: process.env.USER,
-  database: process.env.DB,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
+  user: process.env.DB_USER,
+  database: process.env.DB_DB,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PW,
+  port: process.env.DB_PORT,
 })
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-})
+// pool.on('error', (err, client) => {
+//   console.error('Unexpected error on idle client', err)
+//   process.exit(-1)
+// })
 
 module.exports = pool;
