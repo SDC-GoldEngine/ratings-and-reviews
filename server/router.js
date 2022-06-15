ce8a3c64a060af9758d3acc899b2b63c50691140
Jessica Zhou <jessicazhou541@gmail.com>
2022-06-15 14:16:09 -0700

    placed in app not router

--- server/router.js
+++ server/router.js
@@ -12,8 +12,4 @@ router.put('/reviews/:review_id/helpful', controllers.markHelpful);
 
 router.put('/reviews/:review_id/report', controllers.report);
 
-router.get('/loaderio-3e76a66e6283dac02535a5de95e788c6.txt', (req, res) => {
-  res.status(200).send('loaderio-3e76a66e6283dac02535a5de95e788c6');
-});
-
 module.exports = router;
